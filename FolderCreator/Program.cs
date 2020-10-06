9260cf0f760934d3c5777675b52ec7fb65bd1d96
quanned <avdeyartem1@gmail.com>
2020-10-06 10:00:09 +0300

    fixes

--- FolderCreator/Program.cs
+++ FolderCreator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace FolderCreator
     {
         static void Main(string[] args)
         {
+            string defaultFolderName = "NewFolder";
+            string folderName = Console.ReadLine();
+            if (folderName == "" || folderName == null)
+                folderName = defaultFolderName;
+            
+            string curDate = GetFormatDate();
+            string path = GetFullPath();
+
+            CreateDirectory(path, folderName, curDate);
+        }
+
+        private static string GetFormatDate()
+        {
+            DateTime date = DateTime.Now;
+            return date.ToString("MM-dd-yyyy-hh-mm-ss");
+        }
+
+        private static string GetFullPath()
+        {
+            string path = Environment.CurrentDirectory;
+            return path;
+        }
+
+        private static void CreateDirectory(string _path, string _folderName, string _date)
+        {
+            Directory.CreateDirectory(_path + @"\" + _folderName + "-" + _date);
         }
     }
 }
